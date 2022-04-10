d6639b59bb16ca7421c58b8c16191cd381bbb050
paulito02 <paul.makinde@yahoo.com>
2022-04-11 00:22:17 +0100

    making use of decorator function to beautify a out put message and also to show expression

---
+++ udemy.py
@@ -0,0 +1,21 @@
+# @decorator
+def my_decorator(func):
+    def wrap_func():
+        print('👌👌👌👌👌👌👌👌👌')
+        func()
+        print('👌👌👌👌👌👌👌👌👌')
+
+    return wrap_func
+
+
+@my_decorator
+def hello():
+    print('hello')
+
+
+@my_decorator
+def bye():
+    print('see ya later')
+
+
+bye()
